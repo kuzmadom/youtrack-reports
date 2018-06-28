35ede8bbcbf6bf2ae53d3330fd88f89646c79aa9
Irina Kuzmina <kuzmadom@gmail.com>
2018-06-28 13:43:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,46 @@
-console.log('!');
-
-
-  function extractEmails(text) {
+function extractEmails(text) {
     return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-  }
-
-function compareByLoginsCount(a, b) {
-  if (a.users.length < b.users.length) {
-    return -1;
-  }
-  
-  if (a.users.length > b.users.length) {
-    return 1;
-  }
-  
-  return 0;
 }
 
-  async function getReport(){
+function compareByLoginsCount(a, b) {
+    if (a.users.length < b.users.length) {
+        return -1;
+    }
+
+    if (a.users.length > b.users.length) {
+        return 1;
+    }
+
+    return 0;
+}
+
+async function getReport(){
     const filter = document.getElementsByClassName('js-filter')[0].value;
     const max = 1000;
-    
-    const url = `https://youtrack.moedelo.org/youtrack/rest/issue?filter=${filter}&max=${max}`;
-    const args = { 
-      headers: {
-        Accept: 'application/json',
-        pragma: 'no-cache',
-        'cache-control': 'no-cache', 
-        'Content-Type': 'application/json'
-       }, 
-      credentials: 'include'
-     };
-    
-    const list = (await fetch(url, args)).json().issue;
-    
-    const report = list.map(item => {
-      const title = item.field.find(f => f.name === 'summary').value;
-      return { 
-        name: `${item.id} ${title}`,
-        users: extractEmails(item.comment.map(c => c.text).join(' '))
-      };
-    }));
 
-    console.log(report.sort(compareByLoginsCount);    
-  }
-  
-  
-  const button = document.getElementsByClassName('js-getReportBtn')[0];
-  button.onclick = getReport;
+    const url = `https://youtrack.moedelo.org/youtrack/rest/issue?filter=${filter}&max=${max}`;
+    const args = {
+        headers: {
+            Accept: 'application/json',
+            pragma: 'no-cache',
+            'cache-control': 'no-cache',
+            'Content-Type': 'application/json'
+        },
+        credentials: 'include'
+    };
+
+    const list = (await fetch(url, args)).json().issue;
+
+    const report = list.map(item => {
+        const title = item.field.find(f => f.name === 'summary').value;
+        return {
+            name: `${item.id} ${title}`,
+            users: extractEmails(item.comment.map(c => c.text).join(' '))
+        };
+    });
+
+    console.log(report.sort(compareByLoginsCount);
+}
+
+const button = document.getElementsByClassName('js-getReportBtn')[0];
+button.onclick = getReport;
